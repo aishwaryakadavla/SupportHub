f7144eaccabc4b21b9e7b99c83a5ab11fae8ccad
Aishwarya Kadavla <aishwaryakadavla@gmail.com>
2026-07-22 11:45:03 -0400

    Completed Phase 2 frontend pages

--- tickets/views.py
+++ tickets/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def create_ticket(request):
+    return render(request, "create_ticket.html")
+
+def ticket_list(request):
+    return render(request, "ticket_list.html")
